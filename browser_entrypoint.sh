ca3746a28bd486c8fac54bdf8425597e7e779d95
Ilya Kreymer <ikreymer@gmail.com>
2019-04-01 17:11:15 -0700

    broser entrypoint: improvements for loading CA cert: - if $PROXY_CA_FILE is set and exists as a file, use that as the CA cert - if $PROXY_CA_FILE and $PROXY_CA_URL are set, curl $PROXY_CA_URL and store at $PROXY_CA_FILE, waiting for $PROXY_HOST:$PROXY_PORT to become available

--- browser_entrypoint.sh
+++ browser_entrypoint.sh
@@ -8,6 +8,19 @@ if [[ -n "$PROXY_HOST" ]]; then
         echo "IP: $IP"
     fi
 
+    # if PROXY_CA_FILE is set and does not exist..
+    if [[ -n "$PROXY_CA_FILE" ]] && [ ! -f $PROXY_CA_FILE ]; then
+        if [[ -n "$PROXY_CA_URL" ]]; then
+            # Get CA via specified URL
+            until $(curl --output /dev/null --silent --head --fail "$PROXY_HOST:$PROXY_PORT"); do
+                printf 'Waiting for proxy...'
+                sleep 1
+            done
+
+            curl -x "$PROXY_HOST:$PROXY_PORT"  "$PROXY_CA_URL" > $PROXY_CA_FILE
+        fi
+    fi
+
     export http_proxy=http://$PROXY_HOST:$PROXY_PORT
     export https_proxy=http://$PROXY_HOST:$PROXY_PORT
 fi
